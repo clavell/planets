5d092969552bc8d1797ee80b1c15ec1ed649dbc6
clavell <christopher.lavell@yahoo.ca>
2021-11-29 19:28:32 -0800

    playing around

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,111 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form @submit.prevent="handleSubmit">
+        <label>planet name:</label>
+        <input
+          type="text"
+          v-model="planetname"
+          placeholder="Add Entry"
+          class="field"
+        />
+        <label>planet type:</label>
+        <input
+          type="text"
+          v-model="type"
+          placeholder="Add Entry"
+          class="field"
+        />
+        <label>planet color:</label>
+        <input
+          type="text"
+          v-model="color"
+          placeholder="Add Entry"
+          class="field"
+        />
+        <button type="submit">{{planetButtonText}}!</button>
+
+      </form>
+  <ul>
+    <li v-for="planet in planets" :key="planet.name">
+      <h2>
+        {{ planet.name }}
+        {{ planet.type }}
+        {{ planet.color }}
+      </h2>
+      <button @click="editPlanet(planet)">Edit this planet</button>
+      <button @click="deletePlanet(planet)">Delete this planet</button>
+      
+    </li>
+  </ul>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { useMutation, useQuery, useResult } from '@vue/apollo-composable'
+import allPlanetsQuery from '@/graphql/allPlanets.query.gql' // you can call it whatever you want on import!
+import deletePlanetMutation from '@/graphql/deletePlanet.mutation.gql'
+import createPlanetMutation from '@/graphql/createPlanet.mutation.gql'
+import editPlanetMutation from '@/graphql/editPlanet.mutation.gql'
+import { computed, ref } from 'vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const {result} = useQuery(allPlanetsQuery)
+    const planets = useResult(result, null, data => data.allPlanets.data)
+    const {mutate: deletePlanet} = useMutation(deletePlanetMutation,() => ({
+ 
+      update: (cache, { data: { deletePlanet } }) => {
+        const data = cache.readQuery({ query: allPlanetsQuery })
+        const newData = data.allPlanets.data.filter(planet => planet._id != deletePlanet._id)
+        const newAllPlanets = {...data.allPlanets,data:newData}
+        cache.writeQuery({ query: allPlanetsQuery, data: {...data, allPlanets:newAllPlanets}})
+      },
+    }))
+
+    const { mutate: createPlanet } = useMutation(createPlanetMutation, () => ({
+      update: (cache, { data: { createPlanet } }) => {
+        const data = cache.readQuery({ query: allPlanetsQuery })
+        const newData = [...data.allPlanets.data].concat([createPlanet])
+        const newAllPlanets = {...data.allPlanets,data:newData}
+        cache.writeQuery({ query: allPlanetsQuery, data: {...data, allPlanets:newAllPlanets}})
+      }
+    }))
+
+    const { mutate: updatePlanet } = useMutation(editPlanetMutation)
+
+    const planetname = ref('Mercury')
+    const type = ref("TERRESTRIAL")
+    const color = ref("BROWN")
+
+    const edit = ref(false)
+
+    const planetButtonText = computed(() => {
+      if(edit.value) {
+        return 'Change Planet!'
+      } else {
+        return 'New Planet!'
+      }
+    })
+
+    const handleSubmit = () => {
+      if(edit.value){
+        console.log(edit.value)
+        updatePlanet({id:edit.value, planetData:{name:planetname.value, type:type.value, color:color.value}})
+      }else {
+        createPlanet({planet:{name:planetname.value, type:type.value, color:color.value}})
+      }
+      edit.value = false
+    }
+
+    const editPlanet = (planet) => {
+      edit.value = planet._id
+      planetname.value = planet.name
+      type.value = planet.type
+      color.value = planet.color
+    }
+
+    return {planets, deletePlanet, planetname, handleSubmit, type, color, editPlanet, planetButtonText}
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,301 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import { ApolloClient, createHttpLink, InMemoryCache, } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
 import App from './App.vue'
+import { offlineSecret, onlineSecret } from './secrets';
 
-createApp(App).mount('#app')
+//switch for whehter online or offline database
+const online = false
+
+const secret = online ? onlineSecret : offlineSecret 
+
+var faunadb = require('faunadb'),
+  q = faunadb.query
+
+  var client = new faunadb.Client({
+    secret,
+    domain: online ? 'db.fauna.com' : 'localhost',
+    // NOTE: Use the correct domain for your database's Region Group.
+    scheme: online ? 'https' : 'http',
+    port: online ? 443 : 8443
+  })
+
+
+const { Create, Collection, CreateCollection, Ref, Get, Update,Map, Documents, Paginate,Var,Lambda, Delete, Collections, CreateIndex, Index, Match, Indexes } = q
+
+const PicardId = "316441675078566400"
+const enterpriseId = "316443571709280768"
+const enterpriseDId ="316442075040055808"
+const JanewayId = "316467714116813312"
+const VoyagerId = "316473288806105600"
+const SoloId = "316467779572072960"
+const falconID = "316473370558333440"
+
+//CREATE COLLECTION
+// client.query(CreateCollection({name:"Spaceships"}))
+
+
+//CREATE DOCUMENT
+// client.query(
+// Create(
+//   Collection("Spaceships"),
+//     {
+//       data: {
+//         name: "Falcon",
+//         pilot: Ref(Collection('Pilots'), SoloId)
+//       }
+//     })
+// ).then((res) => {
+//   console.log(res)
+// })
+
+//GET DOCUMENT
+// client.query(
+// Get(Ref(Collection("Spaceships"), enterpriseId))
+// ).then((res) => {
+//   console.log(res)
+// })
+
+//UPDATE DOCUMENT
+// client.query(
+//   Update(Ref(Collection("Spaceships"), enterpriseId),{
+//     data: {
+//       name: 'Enterprise E'
+//     }
+//   })
+// ).then((res) => {
+//   console.log(res)
+// })
+
+//GET EVERYTHING FROM A COLLECTION (this just returns the references)
+// client.query(
+//   Paginate(Documents(Collection("Pilots")))
+// ).then((res) => {console.log(res)})
+
+//GET EVERYTHING FROM A COLLECTION AND RETURN THE INFORMATION INSIDE
+// client.query(
+//   Map(
+//     Paginate(Documents(Collection("Spaceships"))),
+//     Lambda('ref', Get(Var('ref')))
+//   )
+// ).then((res) => {console.log(res)})
+
+//LIST THE COLLECTIONS
+// client.query(Paginate(Collections())).then((res => {console.log(res)}))
+
+//DELETE
+// client.query(
+//   Delete(Ref(Collection('Pilots'), "316467941636833792"))
+// ).then((res) => {
+//   console.log(res)
+// })
+
+//CREATE AN INDEX
+// client.query(
+//   CreateIndex({
+//     name: 'allPilots',
+//     source: Collection('Pilots')
+//   })
+// ).then((res) => {
+//   console.log(res)
+// })
+
+
+//RETRIEVE ALL RESULTS FROM AN INDEX
+// client.query(
+//   Map(
+//     Paginate(Match(Index('allPilots'))),
+//     // (ref) => {return Get(ref)}
+//     Lambda('ref',Get(Var('ref')))
+//   )
+// ).then((res) => {console.log(res)})
+
+//CREATE INDEX THAT SORTS BY NAME
+// client.query(
+//   CreateIndex({
+//     name:'allPilotsSortedByName',
+//     source: Collection("Pilots"),
+//     values: [
+//       {field: ['data', 'name']},
+//       {field: ['ref']}
+//     ]
+//   })
+// ).then((res) => {console.log(res)})
+
+//INDEX THAT RETRIEVES PLANET BY color 
+// client.query(
+//   CreateIndex({
+//     name:'planetByColor',
+//     source: Collection('Planet'),
+//     terms:[
+//       {field: ['data','color']}
+//     ]
+//   })
+//   ).then((res) => {console.log(res)})
+
+//DELETE AN INDEX
+// client.query(
+//   Delete(Index('planetByColor'))
+// ).then((res) => {console.log(res)})
+
+//USE AN INDEX that has an input term
+// client.query(
+//   Map(
+//     Paginate(Match(Index('planetsByType'), 'GAS')),
+//     Lambda('ref', Get(Var('ref')))
+
+//   )
+// ).then((res) => {console.log(res)})
+
+//NEW THOUGHT FOR QUERY
+client.query(
+  Map(
+    Paginate(Match(Index('planetsByType'), 'GAS')),
+    Lambda('ref', Get(Var('ref')))
+
+  )
+).then((res) => {console.log(res)})
+
+
+
+//SHOW ALL INDEXES
+// client.query(
+//   Paginate(Indexes())
+// ).then((res) => {console.log(res)})
+
+
+
+//RETRIEVE RESULTS FROM ANOTHER INDEX
+// client.query(
+//   Paginate(Match(Index('allPilotsSortedByName')))
+// ).then((res) => {
+//   console.log(res)
+// })
+
+
+
+
+
+//HERE IS SOME FAUNADB FQL STUFF THAT I DON'T THINK EVER WORKED
+  // var createP = client.query(
+  //   q.Create(
+  //     q.Collection('Planet'),
+  //     { data: { name:'Mercury',type:'TERRESTRIAL', Color:'BROWN' } }
+  //   )
+  // )
+
+  // var fetchP = client.query(
+  //   q.Map(
+  //     q.Paginate(q.Documents(q.Collection("Planet"))),
+  //     q.Lambda(planet=> q.Get(planet))
+  //   )
+  // )
+
+  // var fetchP = client.query(
+  //     q.Get(q.Ref(q.Collection('Planet'), '316001830436864593'))
+  //   )
+
+  // fetchP.then((ret) => console.log(ret))
+  // .catch((err) => console.error('Error: %s', err))
+//
+
+
+//HERE WE CREATE AN APOLLO CLIENT THAT WORKS
+  const httpLink = createHttpLink({
+    uri: online ? 'https://graphql.fauna.com/graphql' : 'http://localhost:8084/graphql',
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    // const token = localStorage.getItem('token');
+    // const token = 'fnAEYqm3KmACQaJXMvNR-5GykOqml1g-mnS6FCdN' //online secret
+    // const token = 'fnAEYqiyOZACAHeAJMih8z46VBNKnhVxA5QLvLia' //offline secret
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${secret}`,
+      }
+    }
+  });
+  
+const defaultClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+//HERE IS AN ATTEMPTE AT A MUTATION THAT FAILED
+// const PlanetCreate = gql`
+// mutation CreateAPlanet {
+//   createPlanet(data: {
+//   name: "Venus"
+//   type: "TERRESTRIAL"
+//   color: "GREEN"
+//   }) {
+//       name
+//       color
+//   }
+// }
+// `
+
+// defaultClient.mutate({
+//   query: PlanetCreate
+// }).then(res => console.log(res))
+
+//THE BELOW QUERY WORKS
+// const query1 = gql`
+//  query allPlanets{
+//         allPlanets{
+//           data {
+//             name
+//             type
+//             color
+//           }
+//         }
+//       }
+// `
+
+// defaultClient.query({
+//   query: query1
+// }).then(res => console.log(res))
+//ABOVE WORKS UP TO WHERE IT SAYS 'BELOW QUERY WORKS'
+
+//HERE IS A QUERY TO THE GRAPHQL ENDPOINT THAT WORKS WITH JUST THE STANDARD JAVASCRIPT FETCH API
+// fetch('https://graphql.fauna.com/graphql', {
+//   method: 'POST',
+
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: "Bearer fnAEYqm3KmACQaJXMvNR-5GykOqml1g-mnS6FCdN"
+//   },
+
+//   body: JSON.stringify({
+//     query: `
+      // query{
+      //   allPlanets{
+      //     data {
+      //       name
+      //     }
+      //   }
+      // }
+//     `
+//   })
+// })
+// .then(res => {
+//   console.log(res)
+//   console.log(res.json())
+// })
+// .then(data => console.log(data.data))
+
+
+
+
+
+createApp({
+  setup () {
+    provide(DefaultApolloClient, defaultClient)
+  },
+
+  render: () => h(App),
+}).mount('#app')
